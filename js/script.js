642c3d707ad771536165feb5a6f6899cc92b206e
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2021-09-14 20:41:38 +0300

    20 topic

---
+++ js/script.js
@@ -0,0 +1,352 @@
+// function multiply() {
+//   let total = 1;
+
+//   for (const argument of arguments) {
+//     total *= argument;
+//   }
+//   console.log(total);
+//   return total;
+// }
+// multiply(1, 2, 3);
+// // console.log(multiply(1, 2, 3)); //  6
+// // console.log(multiply(1, 2, 3, 4)); //  10
+// // console.log(multiply(1, 2, 3, 4, 5)); //  15
+// Описание объекта
+
+// const apartment = {
+//  imgUrl: "https://via.placeholder.com/640x480",
+//  descr: "Spacious apartment in the city center",
+//  rating: 4,
+//  price: 2153,
+//  tags: ["premium", "promoted", "top"],
+// } ;
+
+// Добавление объекта в объект
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// console.log(lastTag);
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = "Henry Sibola";
+// apartment.tags.push("trusted");
+// console.log(apartment.owner.name);
+
+// Добавление свойств в объект
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//   country: "Jamaica",
+//   city: "Kingston",
+// };
+
+// Короткая запись свойств в объект product
+
+// const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   name,
+//   price,
+//   image,
+//   tags,
+// };
+
+// Добавление в объект свойства имя которого мы точно не знаем
+
+// const emailInputName = "email";
+// const passwordInputName = "password";
+
+// const credentials = {
+//   [emailInputName]: "henry.carter@aptmail.com",
+//   [passwordInputName]: "jqueryismyjam",
+// };
+
+// Перебор объекта циклом for...in
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment) {
+//  keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+// Проверка объекта на собственные свойства hasOwnProperty
+
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   if (apartment.hasOwnProperty(key)) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+//   }
+// }
+
+// Подсчет собственных свойств объекта.
+
+// function countProps(object) {
+//   let propCount = 0;
+
+// for ( const key in object) {
+//  if (object.hasOwnProperty(key)) {
+//   propCount += 1;
+//  }
+// }
+//   return propCount;
+// }
+
+// Вывод свойств и их значений из объекта используя массив for of
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+
+// Подсчет колличества свойств из объекта
+
+// function countProps(object) {
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+//   for (const key of keys) {
+//    propCount += 1;
+//   }
+//   return propCount;
+// }
+// Второй метод
+
+// function countProps(object) {
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+//   propCount = keys.length;
+//   console.log(propCount);
+//   return propCount;
+// }
+
+// Получение массивов свойств и значений из объекта
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+
+// Запись функции расчета зарплат из объекта
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//  const keys = Object.keys(salaries);
+//   for (const key of keys) {
+//   totalSalary += salaries[key];
+//   }
+//   return totalSalary;
+// }
+
+// Перебор массива объекта
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// Перебор массива объекта
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   for (const product of products) {
+//     console.log(product);
+//     if (product.name === productName) {
+//       const getProductPrice = product.price;
+//       return getProductPrice;
+//     }
+//   }
+//   return null;
+// }
+// getProductPrice("Radar");
+
+// Выбор значений определенного ключа из массива объектов
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   const getAllProperty = [];
+//   for (const product of products) {
+//     for (const key in product) {
+//       if (key !== propName) {
+//         continue;
+//       }
+//       getAllProperty.push(product[key]);
+//     }
+//   }
+//   console.log(getAllProperty);
+//   return getAllProperty;
+// }
+
+// getAllPropValues("price");
+
+// const user = {};
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Peter";
+// delete user.name;
+// console.log(user);
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+// let sum = 0;
+// const salaryArray = Object.values(salaries);
+// console.log(salaryArray);
+// for (let salary of salaryArray) {
+//   sum += salary;
+// }
+// console.log(sum);
+
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu",
+// };
+
+// function multiplyNumeric(object) {
+//   for (let key in object) {
+//     if (typeof object[key] === "number") {
+//       object[key] *= 2;
+//     }
+//     continue;
+//   }
+//   console.log(object);
+// }
+
+// multiplyNumeric(menu);
+
+// Перебор массива объектов.
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+
+//  for (let product of products) {
+//    if (product.name === productName) {
+//      return product.price * product.quantity;
+//    }
+//  }
+//   return 0;
+// }
