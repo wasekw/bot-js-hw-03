c46d11ec6ad118feeeb73f8cb2d6bcc9f7f95205
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2021-09-18 22:47:40 +0300

    Ended 3 module

--- js/script.js
+++ js/script.js
@@ -498,3 +498,192 @@
 // const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
 // const bestScore = Math.max(...allScores);
 // const worstScore = Math.min(...allScores);
+
+// Распыление свойств двух объектов при создании нового объекта
+
+// const defaultSettings = {
+//   theme: "light",
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+
+// const finalSettings = { ...defaultSettings, ...overrideSettings };
+// console.log(finalSettings);
+
+// function makeTask(data) {
+//   const completed = false;
+//   const category = "General";
+//   const priority = "Normal";
+//   // Change code below this line
+//   let makeNewTask = {};
+//   makeNewTask = { completed, category, priority, ...data };
+//   return makeNewTask;
+// или так можно сократить код
+// //   return = {completed, category, priority, ...data};
+// // }
+// makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" });
+
+// function add(...args) {
+//   let total = 0;
+//   for (const arg of args) {
+//     total += arg;
+//   }
+//   return total;
+//   // Change code above this line
+// }
+
+// add(74, 11, 62, 46, 12, 36);
+
+// function addOverNum(number, ...args) {
+//   let total = 0;
+
+//   for (const arg of args) {
+//     if (number < arg) {
+//       total += arg;
+//     }
+//   }
+//   return total;
+// }
+// Перебор двух массивов и поиск в них одинаковых элементов и запись их в пустой массив
+// function findEqualObjects(someArray, otherArray) {
+//     var equalObjects = [];
+
+//     someArray.forEach(function(elementOfSomeArray) {
+//         otherArray.forEach(function(elementOfOrherArray){
+//             if (JSON.stringify(elementOfSomeArray) === JSON.stringify(elementOfOrherArray)) {
+//                 equalObjects.push(elementOfOrherArray);
+//             }
+//         });
+//     });
+
+//     return equalObjects;
+
+// function findMatches(array, ...arg) {
+//   const matches = []; // Don't change this line
+//   for (let i = 0; i < array.length; i += 1) {
+//     let a = array[i];
+//     for (let j = 0; j < arg.length; j += 1) {
+//       if (a === arg[j]) {
+//         matches.push(arg[j]);
+//         break;
+//       }
+//     }
+//   }
+//   console.log(matches);
+//   return matches;
+// }
+// findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41);
+// findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2);
+
+// const bookShelf = {
+//   books: ["The last kingdom", "The guardian of dreams"],
+//   getBooks() {
+//     return "Returning all books";
+//   },
+//   addBook(bookName) {
+//     return `Adding book ${bookName}`;
+//   },
+//   updateBook(oldName, newName) {
+//     return `Updating book ${oldName} to ${newName}`;
+//   },
+//   removeBook(bookName) {
+//     return `Deleting book ${bookName}`;
+//   },
+// };
+
+// const bookShelf = {
+//   books: ["The last kingdom", "Haze", "The guardian of dreams"],
+//   updateBook(oldName, newName) {
+//     let index = this.books.indexOf(oldName);
+
+//     this.books.splice(index, 1, newName);
+//   },
+// };
+// bookShelf.updateBook("Haze", "Dungeon chronicles");
+// console.log(bookShelf);
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   getPotions() {
+//     return this.potions;
+//   },
+// };
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   addPotion(potionName) {
+
+//     this.potions.push(potionName);
+//   },
+// };
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   removePotion(potionName) {
+//     const indexPotion = this.potions.indexOf(potionName);
+//     this.potions.splice(indexPotion, 1);
+//   },
+// };
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   updatePotionName(oldName, newName) {
+
+//     const indexChangePotion = this.potions.indexOf(oldName);
+//     this.potions.splice(indexChangePotion, 1, newName);
+//   },
+// };
+
+//  Работа с массивом объектов встроеными методами
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+
+// getPotions() {
+//   return this.potions;
+// },
+// addPotion(newPotion) {
+//   for (const potion of this.potions) {
+//     if (potion.name === newPotion.name) {
+//       return `Error! Potion ${newPotion} is already in your inventory!`;
+//     }
+//   }
+//   this.potions.push(newPotion);
+// },
+// removePotion(potionName) {
+//   let removeIndex;
+//   for (const potion of this.potions) {
+//     if (potion.name === potionName) {
+//       removeIndex = this.potions.indexOf(potion);
+//       this.potions.splice(removeIndex, 1);
+//       break;
+//     }
+//   }
+//   return `Potion ${potionName} is not in inventory!`;
+// },
+// updatePotionName(oldName, newName) {
+//   for (const potion of this.potions) {
+//     if (potion.name === oldName) {
+//       potion.name = newName;
+//       break;
+//     }
+//   }
+//   return `Potion ${oldName} is not in inventory!`;
+// },
+// };
+// atTheOldToad.addPotion({ name: "Dragon breath", price: 700 });
+// atTheOldToad.removePotion("Dragon breath");
+// atTheOldToad.removePotion("Dragon breath");
+// atTheOldToad.addPotion({ name: "Power potion", price: 270 });
+// atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
+// console.log(atTheOldToad);
